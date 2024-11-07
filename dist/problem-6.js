fa33c1d8f349d64942ea3b98e7a9d6bee7c45f36
tonmoyahamed25 <tonmoyahamed2008@gmail.com>
2024-11-08 01:20:28 +0600

    Sixth No Problem Solved

---
+++ dist/problem-6.js
@@ -0,0 +1,9 @@
+"use strict";
+{
+    function updateProfile(profile, updates) {
+        return Object.assign(Object.assign({}, profile), updates);
+    }
+    const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+    console.log(updateProfile(myProfile, { age: 26 }));
+    // ==============================
+}
